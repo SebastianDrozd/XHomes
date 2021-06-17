bb7d5d5c42f10316451a6a50881b37168a873a58
SebastianDrozd <37122892+SebastianDrozd@users.noreply.github.com>
2021-06-17 18:06:04 -0500

    Input field working

--- ClientApp/src/app/estimator/user-info/user-info.component.ts
+++ ClientApp/src/app/estimator/user-info/user-info.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'user-info',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserInfoComponent implements OnInit {
 
-  constructor() { }
+  firstName : string = ""
+  constructor(private router : Router) { }
 
   ngOnInit() {
   }
+
+  
+
+
 
 }
